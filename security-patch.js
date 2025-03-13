3552c85c18154cccfd18e398def151f0d0b07fe9
evalenzuela <ing.valen0421@hotmail.com>
2025-03-12 21:59:01 -0400

    fix: ajustando cambio en el patch

--- security-patch.js
+++ security-patch.js
@@ -1,2 +1,4 @@
 // Corrección de seguridad urgente
 function sanitizeInput(input) { return input.replace(/[<>]/g, ''); }
+// Función de seguridad global
+function validateAllInputs(form) { /* Validación global */ }

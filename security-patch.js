392acc9fd7c27b8dce0f60ede97c4910e7d18678
evalenzuela <ing.valen0421@hotmail.com>
2025-03-12 22:00:19 -0400

    fix: vamos a generar un conflicto ya que hice un cambio tambien en master

--- security-patch.js
+++ security-patch.js
@@ -1,2 +1,4 @@
 // Corrección de seguridad urgente
 function sanitizeInput(input) { return input.replace(/[<>]/g, ''); }
+// Extensión de seguridad
+function secureLoginForm() { /* Seguridad específica para login */ }
